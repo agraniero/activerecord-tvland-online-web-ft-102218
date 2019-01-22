cba6390863eb73d1e4fb7c185bc196dcef06a4f1
Anthony Graniero <ananfro@gmail.com>
2019-01-22 00:45:16 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -8,7 +8,7 @@ class Actor < ActiveRecord::Base
   end
   
   def list_roles
-    characters.collect {|c| "#{c.name}: #{c.show.name}"}
+    characters.collect {|c| "#{c.name} - #{c.show.name}"}
   end
   
 end
